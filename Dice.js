94dc5518df1d89e7a3e4f94acaef2267c08e8800
Hp <Hp@DESKTOP-VQO4T31>
2024-03-17 21:10:54 +0100

    dice game

--- Dice.js
+++ Dice.js
@@ -1,4 +1,5 @@
-
+var score1 = 0;
+var score2 = 0;
 
 function Games() {
     var number = parseInt((Math.random()*6)+1);
@@ -7,14 +8,25 @@ function Games() {
     var dice1 = document.getElementById('player1').src = "images/dice"+number+".png";
     var dice2 = document.getElementById('player2').src = "images/dice"+number1+".png";
 
-    if (dice1> dice2){
-        document.getElementById('result').innerHTML = 'Player 1 Wins <i class="fa fa-facebook-f"></i>';
-    }else if(dice1 == dice2){
-        document.getElementById('result').innerHTML = 'It a tie';
-    }else if(dice1< dice2){
-        document.getElementById('result').innerHTML = 'Player 2 wins';
+
+    while (true){
+        let scores = [];
+        if (dice1> dice2){
+            score1++;
+            scores.push("Player1 "+"("+score1+" - "+score2+")"+" Player2");
+            document.getElementById('result').innerHTML = 'Player 1 Wins'+" 😝";
+            document.getElementById("scores").innerHTML = scores;
+        }else if(dice1 == dice2){
+            scores.push("Player1 "+"("+score1+" - "+score2+")"+" Player2");
+            document.getElementById('result').innerHTML = 'It a tie'+" 🙂";
+            document.getElementById("scores").innerHTML = scores;
+    
+        }else if(dice1< dice2){
+            score2++;
+            scores.push("Player1 "+"("+score1+" - "+score2+")"+" Player2");
+            document.getElementById('result').innerHTML = 'Player 2 wins'+" 🎉";
+            document.getElementById("scores").innerHTML = scores;
+        }
+        return scores;
     }
 }
-
-//store the results
-//add icon
---
+++ try.js
@@ -0,0 +1,6 @@
+let values = [];
+var num = 12;
+var dig = 6;
+values.push("num "+num+" - "+"dig "+dig);
+document.getElementById("try").innerHTML = values;
+console.log(values);
